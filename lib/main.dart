b41a1a67ca41287718247134180e5709ad204b0d
Chris Nzoka-Okoye <chrisnzokaokoye@gmail.com>
2025-02-13 03:43:46 +0100

    created a simple UI for quiz app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,45 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp();
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Column(
+          mainAxisSize: MainAxisSize.min, //centers column content vertically
+          children: [
+            const Text(
+              'Learn Flutter the fun way',
+              style: TextStyle(
+                fontSize: 30,
+                color: Color.fromARGB(
+                  255,
+                  255,
+                  255,
+                  255,
+                ),
+              ),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                side: BorderSide(color: Colors.white),
+              ),
+              child: Text(
+                'Start Quiz',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Color.fromARGB(
+                    255,
+                    255,
+                    255,
+                    255,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
 }
