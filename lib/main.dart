6582d00173caf6fc1f8812e47767ef9c41bb9855
Chris Nzoka-Okoye <chrisnzokaokoye@gmail.com>
2025-02-13 05:18:35 +0100

    updated a simple UI for quiz app

--- lib/main.dart
+++ lib/main.dart
@@ -6,32 +6,23 @@ void main() {
       home: Scaffold(
         body: Container(
           decoration: BoxDecoration(
-              //container holds the background decoration. Thus, we'll remove the background color argument from scaffold widget
-              // gradient: LinearGradient(colors: ),
-              ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min, //centers column content vertically
-            children: [
-              Image.asset('assets/images/quiz-logo.png'),
-              Text(
-                'Learn Flutter the fun way',
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Color.fromARGB(
-                    255,
-                    255,
-                    255,
-                    255,
-                  ),
-                ),
-              ),
-              OutlinedButton(
-                onPressed: () {},
-                style: OutlinedButton.styleFrom(
-                  side: BorderSide(color: Colors.white),
-                ),
-                child: Text(
-                  'Start Quiz',
+            //container holds the background decoration. Thus, we'll remove the background color argument from scaffold widget
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Color.fromARGB(255, 42, 8, 215),
+                  Color.fromARGB(255, 61, 69, 229)
+                ]),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisSize:
+                  MainAxisSize.min, //centers column content vertically
+              children: [
+                Image.asset('assets/images/quiz-logo.png'),
+                Text(
+                  'Learn Flutter the fun way',
                   style: TextStyle(
                     fontSize: 30,
                     color: Color.fromARGB(
@@ -42,8 +33,26 @@ void main() {
                     ),
                   ),
                 ),
-              ),
-            ],
+                OutlinedButton(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: Colors.white),
+                  ),
+                  child: Text(
+                    'Start Quiz',
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Color.fromARGB(
+                        255,
+                        255,
+                        255,
+                        255,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
