8111441678909c90cccf38e3de699375d01340eb
Chris Nzoka-Okoye <chrisnzokaokoye@gmail.com>
2025-02-17 21:30:51 +0100

    separated the content of main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,21 @@ import 'package:quiz_app/start_screen.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(body: HomeScreen()),
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            //container holds the background decoration. Thus, we'll remove the background color argument from scaffold widget
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Color.fromARGB(255, 185, 46, 46),
+                  Color.fromARGB(255, 130, 58, 58)
+                ]),
+          ),
+          child: const HomeScreen(),
+        ),
+      ),
     ),
   );
 }
