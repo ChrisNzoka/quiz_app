037981ada8e6bd38db87c5fccc0911ca31a9961c
Chris Nzoka-Okoye <chrisnzokaokoye@gmail.com>
2025-03-06 05:40:06 +0100

    added stateful widgets

---
+++ lib/quiz.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class QuizCaller extends StatefulWidget {
+  const QuizCaller({super.key});
+
+  @override
+  State<QuizCaller> createState() {
+    return _QuizCallerState();
+  }
+}
+
+class _QuizCallerState extends State<QuizCaller> {
+
+}
+
+@override
+Widget build(context){
+  return()
+}
